3958b80ac68fcf74a398519b3af8a41ace662a47
shoaibibrahim17 <87351965+shoaibibrahim17@users.noreply.github.com>
2026-04-29 12:32:44 +0530

    Update vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // ensuring relative paths on build
+  base: '/ai-maktab/', // ensuring exactly this path on github pages
   plugins: [react()],
 })
